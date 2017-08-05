12b7ab8b3dce4cfa8835b51232297f218d736d5d
Iradukunda Lydia <iralyda@gmail.com>
2017-08-05 19:07:28 +0200

    update database confihurations

--- config/database.php
+++ config/database.php
@@ -56,11 +56,11 @@ return [
 
         'havanao_connection' => [
                     'driver' => 'mysql',
-                    'host' => env('DB_HOST', '127.0.0.1'),
+                    'host' => env('DB_HAVANAO_HOST', '127.0.0.1'),
                     'port' => env('DB_PORT', '3306'),
                     'database' => env('DB_HAVANAO', 'forge'),
-                    'username' => env('DB_USERNAME', 'forge'),
-                    'password' => env('DB_PASSWORD', ''),
+                    'username' => env('DB_HAVANAO_USERNAME', 'forge'),
+                    'password' => env('DB_HAVANAO_PASSWORD', ''),
                     'unix_socket' => env('DB_SOCKET', ''),
                     'charset' => 'utf8mb4',
                     'collation' => 'utf8mb4_unicode_ci',
