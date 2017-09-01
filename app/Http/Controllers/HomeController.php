1b9da6be0061ddecc31f74b9e8b72bda3343bf44
Iradukunda Lydia <iralyda@gmail.com>
2017-09-01 21:09:59 +0200

    Separate Payment Providers Data on the Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use App\Payment;
 use Illuminate\Http\Request;
 
@@ -30,28 +31,54 @@ class HomeController extends Controller
         switch (request()->has('q')) {
             case true:
 
-                $payment  = new Payment;
+                $payment  = new Payment; 
 
                 $payments = $payment->where('subs_account',request()->get('q'))
-                                     ->orWhere('PaymentSpTxId',request()->get('q'))
-                                     ->orderBy('id','DESC')->paginate(20);
+                                     ->orWhere('PaymentSpTxId',request()->get('q'));
 
+                 
+                //$payments = $this->checkmno($payments); 
                 // If we don't have data, then consider archive information 
                 // before displaying by changing connection to archive
                 if ($payments->count() == 0) {
                   $payments = $payment->setConnection('havanao_archive')
                                      ->where('subs_account',request()->get('q'))
-                                     ->orWhere('PaymentSpTxId',request()->get('q'))
-                                     ->orderBy('id','DESC')->paginate(20);  
+                                     ->orWhere('PaymentSpTxId',request()->get('q'));  
                 }
+
 
                 break;
             
             default:
-                $payments = Payment::orderBy('id','DESC')->paginate(20);
+                $payments = new Payment;
                 break;
         }
         
+        $payments = $this->checkmno($payments);
+        $payments = $payments->orderBy('id','DESC')
+                             ->paginate(20);
+
         return view('home',compact('payments'));
+
+    }
+
+    public function checkmno($payments)
+    {
+        $userMno = Auth::user()->mno; // Get MNO
+        $userMno = strtoupper($userMno);
+        $userMno = trim($userMno); // Remove any trailing space
+        if ($userMno =='MTN') {
+            $payments = $payments->where('subs_account','like','25078%');
+        }
+
+        if ($userMno =='TIGO') {
+            $payments = $payments->where('subs_account','like','25072%');
+        }
+
+        if ($userMno =='AIRTEL') {
+            $payments = $payments->where('subs_account','like','25073%');
+        }
+        
+        return $payments;
     }
 }
